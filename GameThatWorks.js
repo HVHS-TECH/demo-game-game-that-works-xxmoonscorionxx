e6770e84ff26871756ca61784cb9d682b064a72f
xxmoonscorionxx <22091wk@hvhs.school.nz>
2025-02-26 20:59:57 +0000

    interesting

--- GameThatWorks.js
+++ GameThatWorks.js
@@ -7,30 +7,75 @@ const COINSIZE = 10;
 const COINTIMEOUT = 4000;
 
 var Score = 0;
+var gameState = "play";
 
 /*******************************************************/
 // setup()
 /*******************************************************/
 function setup() {
+    
 	console.log("setup: ");
 	cnv = new Canvas(WINDOWWIDTH, WINDOWHEIGHT);
 	player = new Sprite(400, 400, 50, 'd');
 	player.color = 'red';
-    createCoin();
-    coins = new group();
+    
+    coins = new Group();
+
+    coins.add(createCoin());
+
+    player.collides(coins, getPoint);
+
+    function getPoint(collider1, collider2) {
+		collider2.remove();
+        Score++;
+	}
 
 }
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
+    
+
+    if(gameState == "play") {
+        runGame();
+    }
+    else if(gameState == "lose") {
+
+    }
+}
+function runGame() {
     background('cyan');
+
+    if (random(0,1000)<15){
+		coins.add(createCoin());
+	}
+
     displayScore();
+
     movePlayer();
+
+    for (var i = 0; i < coins.length; i++){
+		if(checkCoinTime(coins[i])){
+			coins[i].remove();
+			gameState = "lose";
+		}
+	}
+    
+    
+}
+function lose () {
+    background('red');
+    coins.remove();
+    player.remove();
+    fill(0,0,0);
+    textSize(50);
+    text("You missed a coin ");
+    textSize(100);
+    text("Score: " + Score, 10,200);
 }
 
 function removeCoin() {
-
 }
 function displayScore() {
     fill (0,0,0);
@@ -39,13 +84,15 @@ function displayScore() {
 }
 function createCoin() {
     coin = new Sprite(random(0,WINDOWWIDTH),random(0,WINDOWHEIGHT),COINSIZE,'d');
-    coin.color = ("yellow");
+    coin.color = ('yellow');
     coin.coinspawntime = millis();
+    return(coin);
 }
-function checkCoinTime() {
-    if (coin.coinspawntime + COINTIMEOUT < millis()) {
-        coin.remove();
+function checkCoinTime(_coin) {
+    if (_coin.coinspawntime + COINTIMEOUT < millis()) {
+        return(true);
     }
+    return(false);
 }
 function movePlayer() {
     if (kb.pressing('left')) {
